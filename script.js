05c65e82c595b3ac1842d3bb70082e8a9c326237
Abhijith H <abhijithh496@gmail.com>
2022-07-04 22:34:03 +0530

    project completed

--- script.js
+++ script.js
@@ -7,19 +7,13 @@ const wheel = document.querySelector('.wheel');
 const submit = document.querySelector('.submit');
 const sliderval = document.querySelector('.sliderval');
 const slider = document.querySelector('.slider');
-
+const clear = document.querySelector('.clear');
 let color="black";
-let n=40;
+let n = slider.value;
 container.className="container";
 right.appendChild(container);
 createGrid(n);
-slider.oninput = ()=>{
-    deleteGrid(n);
-    n = slider.value;
-    createGrid(n);
-    colorBG();
-    
-}
+
 function deleteGrid(n){
     for (let i = 0; i < `${n}`; i++) {
         container.removeChild(document.querySelector('.row'));
@@ -32,23 +26,44 @@ function createGrid(n){
         rowDiv.className = "row";
         rowDiv.classList.add(`[${i}]`);
         container.appendChild(rowDiv);
+        rowDiv.style.display="flex";
+        rowDiv.style.flex="1";
         for (let j = 0; j < `${n}`; j++) {
             let innerChild = document.createElement('div');
             innerChild.className = "child";
             innerChild.classList.add(`[${i}][${j}]`);
+            
             rowDiv.appendChild(innerChild);
+            innerChild.style.width="100%";
+            innerChild.style.height="100%";
         }
         
     }
 }
-const childs = document.querySelectorAll('.child');
+let childs = document.querySelectorAll('.child');
+clear.addEventListener('click',()=>{
+    childs.forEach(child=>{
+        child.style.cssText ="background-color: white;"
+    })
+})
 submit.addEventListener('click',()=>{
     childs.forEach(child=>{
         child.classList.remove('black');
         child.classList.remove('erase');
         child.classList.remove('rain');
         child.classList.remove('select');
+        child.classList.add('select');
+        colorBG();
+    });
+});
+black.addEventListener('click',()=>{
+    childs.forEach(child=>{
+        child.classList.remove('black');
+        child.classList.remove('erase');
+        child.classList.remove('rain');
+        child.classList.remove('select');
         child.classList.add('black');
+        colorBG();
     });
 });
 rain.addEventListener('click',()=>{
@@ -58,6 +73,7 @@ rain.addEventListener('click',()=>{
         child.classList.remove('rain');
         child.classList.remove('select');
         child.classList.add('rain');
+        colorBG();
     });
 });
 
@@ -68,9 +84,11 @@ eraser.addEventListener('click',()=>{
         child.classList.remove('rain');
         child.classList.remove('select');
         child.classList.add('erase');
+        colorBG();
     });
 });
 function colorBG(){
+    childs = document.querySelectorAll('.child');
     childs.forEach(child => {
         child.onmouseover = ()=>{
             console.log("test");
@@ -80,6 +98,8 @@ function colorBG(){
             color="white";
         }else if(child.classList.contains('rain')){
             color=`#${Math.floor(Math.random()*16777215).toString(16)}`
+        }else if(child.classList.contains('select')){
+            color = `${wheel.value}`;
         }
         child.style.cssText = `background-color: ${color};`
     }
@@ -89,3 +109,11 @@ function colorBG(){
 });
 }
 colorBG();
+slider.oninput = ()=>{
+    deleteGrid(n);
+    n = slider.value;
+    createGrid(n);
+    colorBG();
+    sliderval.textContent=`${n} x ${n}`
+    
+}
